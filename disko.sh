351f7a52313548e790f333b36b7177fcf2bec2fb
Nima Saed <nima@nmsd.xyz>
2025-07-06 21:30:11 +0200

    Added disko command as disko.sh

---
+++ disko.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o nounset # Treat unset variables as an error
+
+
+sudo nix \
+  --experimental-features "nix-command flakes" \
+  run github:nix-community/disko/latest -- \
+  --mode destroy,format,mount \
+  ./hosts/vm/disko.nix
