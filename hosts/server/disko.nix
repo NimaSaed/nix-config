c7717575ecaf6f32167c6b16faf80366787441e6
Nima Saed <nima@nmsd.xyz>
2025-10-25 21:56:42 +0200

    testing disco

--- hosts/server/disko.nix
+++ hosts/server/disko.nix
@@ -1,20 +1,16 @@
 {
   disko.devices = {
-    disk = {
-      main = {
-        device = "/dev/sdb";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/data";
-              };
-            };
+    disk.data = {
+      type = "disk";
+      device = "/dev/sdb";
+      content = {
+        type = "gpt";
+        partitions.data = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/data";
           };
         };
       };
