5016264c03bd49dd846e72f4a0080f5a70181f93
Nima Saed <nima@nmsd.xyz>
2025-10-26 14:10:11 +0100

    updated server hardware config

--- hosts/server/hardware-configuration.nix
+++ hosts/server/hardware-configuration.nix
@@ -5,28 +5,42 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/28d6e4fd-9b6c-4487-bca7-7c79d5a76948";
-      fsType = "ext4";
+    { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
+      fsType = "btrfs";
+      options = [ "subvol=rootfs" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5362-FB08";
+    { device = "/dev/disk/by-uuid/811E-7A22";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/d9fd397c-2e15-41c9-9119-807c7e9e6d9f"; }
-    ];
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  boot.swraid.enable = true;
+
+
 }
