46c8136150a0fde349f8fdbbd2f7b819be9cca21
Nima Saed <nima@nmsd.xyz>
2025-10-26 14:34:58 +0100

    commented the filesystem

--- hosts/server/hardware-configuration.nix
+++ hosts/server/hardware-configuration.nix
@@ -10,31 +10,31 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
-      fsType = "btrfs";
-      options = [ "subvol=rootfs" ];
-    };
+  #fileSystems."/" =
+  #  { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
+  #    fsType = "btrfs";
+  #    options = [ "subvol=rootfs" ];
+  #  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/811E-7A22";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  #fileSystems."/boot" =
+  #  { device = "/dev/disk/by-uuid/811E-7A22";
+  #    fsType = "vfat";
+  #    options = [ "fmask=0077" "dmask=0077" ];
+  #  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
-      fsType = "btrfs";
-      options = [ "subvol=home" ];
-    };
+  #fileSystems."/home" =
+  #  { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
+  #    fsType = "btrfs";
+  #    options = [ "subvol=home" ];
+  #  };
 
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
-      fsType = "btrfs";
-      options = [ "subvol=nix" ];
-    };
+  #fileSystems."/nix" =
+  #  { device = "/dev/disk/by-uuid/e664545d-9ee4-4e4a-a8cd-dfb9c9ba49c9";
+  #    fsType = "btrfs";
+  #    options = [ "subvol=nix" ];
+  #  };
 
-  swapDevices = [ ];
+  #swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
 
