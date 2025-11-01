f068b3a959d98157a9144163eb8ca75a16a5ae90
Nima Saed <nima@nmsd.xyz>
2025-11-01 22:04:30 +0100

    Updated cockpit setting

--- hosts/chestnut/default.nix
+++ hosts/chestnut/default.nix
@@ -78,7 +78,7 @@
     enable = true;
     port = 9090;
     openFirewall = true;
-    allowed-origins = [ "chestnut:9090", "chestnut.nmsd.xyz:9090" ];
+    allowed-origins = [ "chestnut:9090" "chestnut.nmsd.xyz:9090" ];
     settings = {
       WebService = {
         AllowUnencrypted = true;
