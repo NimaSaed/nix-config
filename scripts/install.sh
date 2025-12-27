8e74706215683908b99abe76eff0c287dbe306b7
Nima Saed <nima@nmsd.xyz>
2025-12-27 14:18:36 +0100

    Changed the installation script user to root.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ set -o nounset # Treat unset variables as an error
 
 
 
-nix run github:numtide/nixos-anywhere -- --build-on remote --flake .#chestnut nixos@chestnut.nmsd.xyz
+nix run github:numtide/nixos-anywhere -- --build-on remote --flake .#chestnut root@chestnut.nmsd.xyz
