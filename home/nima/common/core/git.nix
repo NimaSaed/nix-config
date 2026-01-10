c0f52c1328919f60d8753e9dde304b9515914169
Nima Saed <nima@nmsd.xyz>
2026-01-10 23:37:51 +0100

    fixed some warning in colmena

--- home/nima/common/core/git.nix
+++ home/nima/common/core/git.nix
@@ -3,23 +3,22 @@
 {
   programs.git = {
     enable = true;
-    userName = "Nima"; # TODO: Update with your name
-    userEmail = "nima@example.com"; # TODO: Update with your email
-
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Nima";
+        email = "nima@nmsd.xyz";
+      };
       init.defaultBranch = "main";
       pull.rebase = false;
       core.editor = "vim";
-    };
-
-    # Useful git aliases
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      ci = "commit";
-      lg =
-        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        lg =
+          "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
     };
   };
 }
