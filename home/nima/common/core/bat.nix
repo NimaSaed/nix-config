d7c9d257bb3e0f649281401f0b1a27bde0d21a9a
Nima Saed <nima@nmsd.xyz>
2026-01-11 18:53:27 +0100

    Disable bat cache build - workaround for memory allocation bug.

--- home/nima/common/core/bat.nix
+++ home/nima/common/core/bat.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
+  # Disable bat cache build - workaround for memory allocation bug
+  # See: https://discourse.nixos.org/t/bat-is-no-longer-working/38960
+  home.activation.batCache = lib.mkForce "";
+
   programs.bat = {
     enable = true;
 
