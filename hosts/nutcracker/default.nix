4c4f24c3aae8f5a48d6de43ba330c51f6f74964f
Nima Saed <nima@nmsd.xyz>
2026-01-12 20:59:11 +0100

    Disabled authelia and lldap

--- hosts/nutcracker/default.nix
+++ hosts/nutcracker/default.nix
@@ -20,11 +20,11 @@
     };
     tools.enable = true;
     media.enable = true;
-    auth = {
-      enable = false;
-      authelia.enable = true;
-      lldap.enable = true;
-    };
+    #auth = {
+    #  enable = true;
+    #  authelia.enable = true;
+    #  lldap.enable = true;
+    #};
   };
 
   # ============================================================================
