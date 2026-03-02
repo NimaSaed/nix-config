4c44c3f15bba2ea1540c9997553dae9311ca11d1
Nima Saed <nima@nmsd.xyz>
2026-03-02 20:35:21 +0100

    Added firefox to mac

--- home/nima/mac.nix
+++ home/nima/mac.nix
@@ -53,6 +53,7 @@
     brave # Web browser
     monitorcontrol # External monitor controls
     zoom-us # Video conferencing
+    firefox
   ];
 
   # ===========================================================================
