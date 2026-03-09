2a9e027aab8b458c1694399a77a083021db3044b
Nima Saed <nima@nmsd.xyz>
2026-03-09 20:20:18 +0100

    updated alacritty theme, and added .sync-exclude.lst so nextcloud sync ignores .git folder.

--- home/nima/common/optional/alacritty.nix
+++ home/nima/common/optional/alacritty.nix
@@ -69,10 +69,41 @@ let
         white = "#fdf6e3";
       };
     };
+
+    tomorrow_night = {
+      cursor = {
+        text = "#1d1f21";
+        cursor = "#c5c8c6";
+      };
+      primary = {
+        background = "#1d1f21";
+        foreground = "#c5c8c6";
+      };
+      normal = {
+        black = "#282a2e";
+        red = "#a54242";
+        green = "#8c9440";
+        yellow = "#de935f";
+        blue = "#5f819d";
+        magenta = "#85678f";
+        cyan = "#5e8d87";
+        white = "#707880";
+      };
+      bright = {
+        black = "#373b41";
+        red = "#cc6666";
+        green = "#b5bd68";
+        yellow = "#f0c674";
+        blue = "#81a2be";
+        magenta = "#b294bb";
+        cyan = "#8abeb7";
+        white = "#c5c8c6";
+      };
+    };
   };
 
   # Change this to switch themes
-  activeTheme = "solarized_light";
+  activeTheme = "tomorrow_night";
 in
 {
   programs.alacritty = {
