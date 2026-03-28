d8a5274fb102a614d4b5e84dbd792c882db58d0a
Nima Saed <nima@nmsd.xyz>
2026-03-28 21:11:27 +0100

    fixed - add WHITEBOARD_JWT_SECRET to nextcloud-app container to setup whiteboard

--- modules/podman/pod-nextcloud.nix
+++ modules/podman/pod-nextcloud.nix
@@ -510,7 +510,7 @@ in
                     # Whiteboard: set backend URL and JWT secret
                     occ app:enable whiteboard
                     occ config:app:set whiteboard collabBackendUrl --value "https://${cfg.whiteboard.subdomain}.${domain}"
-                    occ config:app:set whiteboard jwt_secret_key --value "$(grep '^WHITEBOARD_JWT_SECRET=' ${nixosConfig.sops.templates."nextcloud-app-secrets".path} | cut -d= -f2- | tr -d '\n')"
+                    occ config:app:set whiteboard jwt_secret_key --value "$(${pkgs.gnugrep}/bin/grep '^WHITEBOARD_JWT_SECRET=' ${nixosConfig.sops.templates."nextcloud-app-secrets".path} | ${pkgs.coreutils}/bin/cut -d= -f2- | ${pkgs.coreutils}/bin/tr -d '\n\r')"
                   ''}
                 ''}";
               };
