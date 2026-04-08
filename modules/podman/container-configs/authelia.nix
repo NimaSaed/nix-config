82a1e1ee506f700500923e185952feab217de7e1
Nima Saed <nima@nmsd.xyz>
2026-04-08 09:59:29 +0200

    vaultwarden: use SSO_AUTH_ONLY_NOT_SESSION for reliable session persistence

--- modules/podman/container-configs/authelia.nix
+++ modules/podman/container-configs/authelia.nix
@@ -315,12 +315,10 @@ in
                 - openid
                 - profile
                 - email
-                - offline_access
               response_types:
                 - code
               grant_types:
                 - authorization_code
-                - refresh_token
               access_token_signed_response_alg: none
               userinfo_signed_response_alg: none
               token_endpoint_auth_method: client_secret_basic
